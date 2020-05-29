cfee5b4b33f127e43cc014f04b151dd39437bf8d
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2020-05-28 22:01:44 -0500

    Main

---
+++ Main.py
@@ -0,0 +1,46 @@
+class Grammar():
+    def __init__(self, notTerminal, terminal, start, productions):
+        self.not_Terminal=notTerminal
+        self.terminal=terminal
+        self.start=start
+        self.productions=productions
+    def MakeTree(self, string):
+        return
+
+def ChooseFile():
+    return "test" + input("Select a test file: ") + ".txt"
+
+
+def ReadFile(path):
+    file = open(path, "r")
+    lines = file.readlines()
+    # print(lines)
+    file.close()
+
+    # Remove the enter in every line
+    for i in range(len(lines)):
+        lines[i] = lines[i].replace("\n", "")
+
+    # Not-terminal symbols alphabet
+    not_Terminal = lines[0].split(",")
+    print(not_Terminal)
+
+    # Terminal symbols alphabet
+    terminal = lines[1].split(",")
+    print(terminal)
+
+    # Starting non-terminal symbol
+    start = lines[2]
+    print(start)
+
+    # Productions
+    productions=[]
+    for i in range(3, len(lines)):
+        temp = lines[i].split("->")
+        productions.append((temp[0], temp[1]))
+    print(productions)
+
+    return Grammar(not_Terminal,terminal,start,productions)
+
+grammar=ReadFile(ChooseFile())
+print()
