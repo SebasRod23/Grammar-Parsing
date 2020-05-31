59ce65b0f052a373450d83ae118a9b87ed48c15e
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2020-05-30 20:35:32 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ from Tree import Tree
 from tkinter import *
 from tkinter import messagebox
 
+# Default path and type assignation
 path = "test1.txt"
 grammar: Grammar
 tree: Tree
@@ -14,7 +15,7 @@ def Render(string, height):
     grammar = ReadFile(path)
     if CheckString(string, grammar.terminal):
         print("-----------------------------------------")
-        print("Creating parsing-tree for: " + string)
+        print("Creating parsing-tree for: " + string+", with height: {}".format(height))
         print("-----------------------------------------")
         # Make tree
         tree = grammar.MakeTree(string, int(height))
@@ -88,9 +89,12 @@ def ReadFile(path):
 
     return Grammar(not_Terminal, terminal, Node(start, None), productions)
 
+# Window script
 
+# Color for buttons when selected
 colorBottom = "#282626"
 
+# Create the Main Window for the app
 mainWindow = Tk()
 mainWindow.title("Parsing Tree")
 screen_width = mainWindow.winfo_screenwidth() - 50
@@ -99,15 +103,17 @@ mainWindow.geometry("{}x{}".format(screen_width, screen_height))
 mainWindow.resizable(0, 0)
 mainWindow.iconbitmap(default="icon.ico")
 
+# Create the Canvas where the tree will be rendered
 canvas = Canvas(mainWindow, bg="#000000", height=screen_height - 50, bd=0, highlightthickness=0, relief='ridge')
 canvas.pack(side=TOP, fill=X)
 
+# Create the bottom frame for the buttons and inputs
 infoFrame = Frame(mainWindow, bg=colorBottom, height=50)
 infoFrame.pack(side=BOTTOM, fill=X)
 
+# File selection label with instructions and buttons with their corresponding files
 lblSelect = Label(infoFrame, text="Select an input file:", height=50, bg=colorBottom, fg="white")
 lblSelect.pack(side=LEFT, padx=(20, 0))
-
 bttnOne = Button(infoFrame, text="File 1", width=15, height=40, bg="#625E5E", command=lambda: ChooseFile(1))
 bttnOne.pack(side=LEFT, padx=(20, 0), pady=5)
 bttnTwo = Button(infoFrame, text="File 2", width=15, height=40, command=lambda: ChooseFile(2))
@@ -117,16 +123,19 @@ bttnThree.pack(side=LEFT, pady=5)
 bttnFour = Button(infoFrame, text="File 4", width=15, height=40, command=lambda: ChooseFile(4))
 bttnFour.pack(side=LEFT, pady=5)
 
+# Entry for string input
 lblString = Label(infoFrame, text="String:", height=50, bg=colorBottom, fg="white")
 lblString.pack(side=LEFT, padx=(40, 0))
 txtString = Entry(infoFrame, width=30)
 txtString.pack(side=LEFT, padx=(5, 0))
 
+# Spinbox for height of the tree
 lblHeight = Label(infoFrame, text="Height:", height=50, bg=colorBottom, fg="white")
 lblHeight.pack(side=LEFT, padx=(20, 0))
 spbHeight = Spinbox(infoFrame, from_=1, to=10)
 spbHeight.pack(side=LEFT, padx=(10, 0))
 
+# Button to Render the tree
 bttnRender = Button(infoFrame, text="Render", width=15, height=40,
                     command=lambda: Render(txtString.get(), spbHeight.get()))
 bttnRender.pack(side=RIGHT, padx=(0, 30), pady=5)
